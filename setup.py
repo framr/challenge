4793821cac79588b11cc6ce7405bdc7d79eefef2
framr <pavel.sht@yandex.ru>
2016-12-25 14:27:28 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+import shlex
+import sys
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+with open('requirements.txt') as fd:
+    requirements = [line.rstrip() for line in fd]
+
+with open('test_requirements.txt') as fd:
+    test_requirements = [line.rstrip() for line in fd]
+
+
+class PyTest(TestCommand):
+
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = '--cov=outbrain_flowkeeper'
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
+
+
+setup(name='outbrain',
+      version='0.0.1',
+      author='fram',
+      author_email='pavel.sht@yandex.ru',
+      install_requires=requirements,
+      tests_require=test_requirements,
+      cmdclass={'test': PyTest},
+      packages=find_packages()
+)
