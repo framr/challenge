555ebf9221c0c2203cf30f460cb48bf57329190c
framr <pavel.sht@yandex.ru>
2016-12-27 12:26:57 +0300

    bugfixes in script for merging files

--- outbrain/scripts/merger.py
+++ outbrain/scripts/merger.py
@@ -30,11 +30,17 @@ def add_columns_and_strip_header(infilename, outfilename, add_columns=None, sepa
             header = infile.readline().strip().split(separator)
 
             column_names, column_values = zip(*add_columns)
+            column_names = list(column_names)
+            column_values = list(column_values)
+
+#            print add_columns
+#            print column_names, column_values
             header.extend(column_names)
 
             writer = csv.writer(outfile)
-            for row in csv.reader:
+            for row in csv.reader(infile):
                 writer.writerow(row + column_values)
+                print row + column_values
 
     return header
 
@@ -43,13 +49,15 @@ if __name__ == '__main__':
 
 
     argparser = ArgumentParser()
-    argparser.add_argument("--train", dest="train", type=str, default=None, help="first file")
-    argparser.add_argument("--test", dest="test", type=str, default=None, help="second file")
-    argparser.add_argument("--out", dest="output", type=str, default=None, help="output file")
+    argparser.add_argument("--train", dest="train", type=str, default=None, help="first file", required=True)
+    argparser.add_argument("--test", dest="test", type=str, default=None, help="second file", required=True)
+    argparser.add_argument("--out", dest="output", type=str, default=None, help="output file", required=True)
     argparser.add_argument("--key", dest="sort_column", type=str, default=None,
-                           help="column used for sorting")
+                           help="column used for sorting", required=True)
     argparser.add_argument("--sort-args", dest="sort_args", type=str, default="-g",
                            help="additional sort arguments")
+    argparser.add_argument("--separator", dest="separator", type=str, default=",",
+                           help="csv file separator")
 
     args = argparser.parse_args()
 
@@ -57,11 +65,15 @@ if __name__ == '__main__':
     with NamedTemporaryFile() as train:
         with NamedTemporaryFile() as test:
             train_header = add_columns_and_strip_header(
-                args.train, train.name, add_columns=[("is_train", "1")]
+                args.train, train.name, add_columns=[["is_train", "1"]]
             )
             test_header = add_columns_and_strip_header(
-                args.test, test.name, add_columns=[("clicked", "0"), ("is_train", "0")]
+                args.test, test.name, add_columns=[["clicked", "0"], ["is_train", "0"]]
             )
+
+            print "Resulting train header %s" % train_header
+            print "Resulting test header %s" % test_header
+
 
             if train_header != test_header:
                 raise ValueError("headers mismatch %s for train vs %s for test"
@@ -70,13 +82,16 @@ if __name__ == '__main__':
             columns_mapping = dict((column, index + 1) for index, column in enumerate(train_header))
             sort_column_index = columns_mapping[args.sort_column]
 
-            with open(args.out, "w") as outfile:
+            with open(args.output, "w") as outfile:
                 outfile.write("%s\n" % SEPARATOR.join(train_header))
 
+
+            sort_cmd = 'sort %s -k %d,%d --field-separator="%s" >> %s' % (
+                args.sort_args, sort_column_index, sort_column_index, args.separator, args.output)
+            print "command for sorting: %s" % sort_cmd
+
             proc1 = Popen("cat %s %s" % (train.name, test.name), stdout=PIPE, shell=True)
-            proc2 = Popen("sort -k %d,%d %s >> %s"
-                          % (sort_column_index, sort_column_index, args.sort_args, args.out),
-                          stdin=proc1.stdout, shell=True)
+            proc2 = Popen(sort_cmd, stdin=proc1.stdout, shell=True)
 
             proc1.stdout.close() # Allow proc1 to receive a SIGPIPE if proc2 exits. WTF???
             stdout, stderr = proc2.communicate()
