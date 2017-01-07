2f52150f1990462679a9867ec45387f67ffaf201
framr <pavel.sht@yandex.ru>
2017-01-07 20:06:34 +0300

    add metric averages over number of groups

--- outbrain/metrics/common.py
+++ outbrain/metrics/common.py
@@ -18,16 +18,19 @@ class MetricsGroupAggregator(object):
 
         metrics = dict((ev.NAME, 0) for ev in self._evaluators)
         examples_count = 0
+        groups_count = 0
         with open(infilename) as infile:
             for group_key, examples_batch in csv_file_group_iter(infile,
                                                                  group_field=self._group_field):
 
                 examples_count += len(examples_batch)
+                groups_count += 1
                 for evaluator in self._evaluators:
                     metrics[evaluator.NAME] += evaluator(examples_batch)
 
         for metric_name in metrics.keys():
             metrics["avg_%s" % metric_name] = metrics[metric_name] / float(examples_count)
+            metrics["avg_group_%s" % metric_name] = metrics[metric_name] / float(groups_count)
 
         return metrics
 
